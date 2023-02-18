340152c8f7888d7e5aa23f15c2c80ffcac97a9a9
Kostya Sergienko <sergienko.kostya@ukr.net>
2023-02-18 21:46:12 +0200

    kostya3

--- src/main.js
+++ src/main.js
@@ -13,3 +13,20 @@
     refs.modal.classList.toggle("is-booking-hidden");
   }
 })();
+
+// ===============МОДАЛКА ДЛЯ PRICE-ЦІНА НА НОМЕР
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[price-modal-open]"),
+    closeModalBtn: document.querySelector("[price-modal-close]"),
+    modal: document.querySelector("[price-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-price-hidden");
+  }
+})();
