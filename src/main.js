e6741901b8c0d038984189444f075c5125c593db
Oleksandr Tymoshenko <hermeusmanua@gmail.com>
2023-02-21 15:28:21 +0200

    add modal to suite section

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 // ===============МОДАЛКА ДЛЯ BOOKING-ЗАБРОНЮВАТИ НОМЕР
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[booking-modal-open]"),
-    closeModalBtn: document.querySelector("[booking-modal-close]"),
-    modal: document.querySelector("[booking-modal]"),
+    openModalBtn: document.querySelector('[booking-modal-open]'),
+    closeModalBtn: document.querySelector('[booking-modal-close]'),
+    modal: document.querySelector('[booking-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-booking-hidden");
+    refs.modal.classList.toggle('is-booking-hidden');
   }
 })();
 // =========================================================================
@@ -18,62 +18,60 @@
 // ДВОХМІСНИЙ ЛЮКС
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[price-modal-open]"),
-    closeModalBtn: document.querySelector("[price-modal-close]"),
-    modal: document.querySelector("[price-modal]"),
+    openModalBtn: document.querySelector('[price-modal-open]'),
+    closeModalBtn: document.querySelector('[price-modal-close]'),
+    modal: document.querySelector('[price-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-price-hidden");
+    refs.modal.classList.toggle('is-price-hidden');
   }
 })();
 // ТРОХМІСНИЙ
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[price-thre-modal-open]"),
-    closeModalBtn: document.querySelector("[price-thre-modal-close]"),
-    modal: document.querySelector("[price-modal]"),
+    openModalBtn: document.querySelector('[price-thre-modal-open]'),
+    closeModalBtn: document.querySelector('[price-thre-modal-close]'),
+    modal: document.querySelector('[price-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-price-hidden");
+    refs.modal.classList.toggle('is-price-hidden');
   }
 })();
 // ДВУХМІСНИЙ
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[price-two-modal-open]"),
-    closeModalBtn: document.querySelector("[price-two-modal-close]"),
-    modal: document.querySelector("[price-modal]"),
+    openModalBtn: document.querySelector('[price-two-modal-open]'),
+    closeModalBtn: document.querySelector('[price-two-modal-close]'),
+    modal: document.querySelector('[price-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-price-hidden");
+    refs.modal.classList.toggle('is-price-hidden');
   }
 })();
 // ===============МОДАЛКА ДЛЯ BOOKING-ЗАБРОНЮВАТИ СТОЛИК
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
-
-
